b69860c41843a16fd3b2640797030153757ceac1
Severin Spoerri <severin.spoerri@outlook.com>
2021-11-03 00:04:08 +0100

    initial commit
    scss setup
    carbon components setup
    all css styles import

---
+++ src/routes/index.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { DatePicker, DatePickerInput } from 'carbon-components-svelte';
+</script>
+
+<svelte:head>
+	<title>Mutoco-moco</title>
+</svelte:head>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h2 class="test">Installation</h2>
+
+<DatePicker datePickerType="single" on:change>
+	<DatePickerInput labelText="Meeting date" placeholder="mm/dd/yyyy" />
+</DatePicker>
+
+<style lang="scss">
+	.test {
+		color: red;
+
+		&:hover {
+			color: blue;
+		}
+	}
+</style>
