ad61a4e569e605d29868a683a3aa0b4851e6e285
Severin Spoerri <severin.spoerri@outlook.com>
2021-11-07 18:58:14 +0100

    date functionality

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,6 +12,10 @@
 	const minDate = new Date();
 	const maxDate = eventDate;
 
+	const daysUntilEvent = Math.floor(
+		(eventDate.getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24)
+	);
+
 	let isFormValid: boolean = false;
 
 	let date: Date | string = new Date();
@@ -19,6 +23,8 @@
 	let hasTimeError: boolean = false;
 
 	let time = '12:00';
+
+	let reminderDays: number = 0;
 
 	// regex pattern for european date format
 	// const datePattern =
@@ -42,6 +48,35 @@
 	function isValidTime(timeString: string): boolean {
 		const timeRegex = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
 		return timeRegex.test(timeString);
+	}
+
+	function updateDaysUntilEvent(e: CustomEvent<any>) {
+		reminderDays =
+			(e.detail.selectedDates[0].getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24);
+	}
+
+	function increaseReminderDays(): void {
+		// get remaining days until event
+
+		if (reminderDays < daysUntilEvent) {
+			reminderDays++;
+
+			// increase date by one day
+			if (date instanceof Date) {
+				date = new Date(date.setDate(date.getDate() + 1));
+			}
+		}
+	}
+
+	function decreaseReminderDays(): void {
+		if (reminderDays > 0) {
+			reminderDays--;
+			if (date instanceof Date) {
+				// add one day to date
+				date = new Date(date.setDate(date.getDate() - 1));
+				console.log(date);
+			}
+		}
 	}
 
 	function handleSubmit(e) {
@@ -68,6 +103,7 @@
 
 <form on:submit|preventDefault={handleSubmit}>
 	<DatePicker
+		on:change={updateDaysUntilEvent}
 		datePickerType="single"
 		bind:value={date}
 		dateFormat="d.m.Y"
@@ -93,8 +129,47 @@
 		</TimePickerSelect> -->
 	</TimePicker>
 
+	<div class="counter">
+		<button
+			disabled={reminderDays <= 0}
+			type="button"
+			on:click={decreaseReminderDays}
+			aria-label="Decrease the counter by one"
+		>
+			<svg aria-hidden="true" viewBox="0 0 1 1">
+				<path d="M0,0.5 L1,0.5" />
+			</svg>
+		</button>
+
+		<div class="counter-viewport">
+			<div class="counter-digits">
+				in {reminderDays} Days
+			</div>
+		</div>
+
+		<button
+			disabled={reminderDays >= daysUntilEvent}
+			type="button"
+			on:click={increaseReminderDays}
+			aria-label="Increase the counter by one"
+		>
+			<svg aria-hidden="true" viewBox="0 0 1 1">
+				<path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
+			</svg>
+		</button>
+	</div>
+
 	<button class="button" type="submit" disabled={isFormValid}>Submit</button>
 </form>
 
 <style lang="scss">
+	svg {
+		width: 25%;
+		height: 25%;
+	}
+	path {
+		vector-effect: non-scaling-stroke;
+		stroke-width: 2px;
+		stroke: rgb(177, 125, 125);
+	}
 </style>
