1e99a0db7f07d7b15d483f7444c7d73539261a35
Severin Spoerri <severin.spoerri@outlook.com>
2021-11-07 02:02:01 +0100

    date and timepicker

---
+++ src/lib/dates.ts
@@ -0,0 +1,3 @@
+const date = new Date('12-17-2021 18:00');
+
+date;
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,38 @@
 <script lang="ts">
 	import { DatePicker, DatePickerInput } from 'carbon-components-svelte/src/DatePicker';
+	import { TimePicker, TimePickerSelect } from 'carbon-components-svelte/src/TimePicker';
+	import { SelectItem } from 'carbon-components-svelte/src/Select';
+
 	// import { DatePicker } from 'attractions';
+
+	const eventDate = new Date('12-17-2021 18:00');
+	const eventDateString = eventDate.toDateString();
+	const eventTimeString = eventDate.toLocaleTimeString();
+
+	const minDate = new Date();
+	const maxDate = eventDate;
+
+	let invalid: boolean = false;
+
+	let value: Date = new Date();
 </script>
 
 <svelte:head>
 	<title>Mutoco-moco</title>
 </svelte:head>
 
-<DatePicker datePickerType="single" on:change dateFormat="d.m.Y">
-	<DatePickerInput placeholder="dd.mm.yyyy" />
+<div>{eventDateString} | {eventTimeString}</div>
+
+<DatePicker datePickerType="single" bind:value dateFormat="d.m.Y" {maxDate} {minDate}>
+	<DatePickerInput placeholder="Bitte Datum eingeben" />
 </DatePicker>
 
-<div class="container">
-	<h1 class="test">Fucker</h1>
-</div>
+<TimePicker placeholder="hh:mm">
+	<!-- <TimePickerSelect value="PM">
+		<SelectItem value="am" text="AM" />
+		<SelectItem value="pm" text="PM" />
+	</TimePickerSelect> -->
+</TimePicker>
 
 <!-- <input type="date" class="input-date" title="date" aria-label="Reminder Date" /> -->
 
